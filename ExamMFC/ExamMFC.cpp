9ad5f8d94a0c492cfcad0bcd17304924c7475e25
yerimkang <yerim2300@gmail.com>
2024-03-25 11:15:42 +0900

    대화상자 예제
    https://www.youtube.com/watch?v=8MQsJDk5HTk

---
+++ ExamMFC/ExamMFC.cpp
@@ -0,0 +1,49 @@
+﻿
+// ExamMFC.cpp: 애플리케이션에 대한 클래스 동작을 정의합니다.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "ExamMFC.h"
+#include "ExamMFCDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CExamMFCApp
+
+BEGIN_MESSAGE_MAP(CExamMFCApp, CWinApp)
+	ON_COMMAND(ID_HELP, &CWinApp::OnHelp)
+END_MESSAGE_MAP()
+
+
+// CExamMFCApp 생성
+
+CExamMFCApp::CExamMFCApp()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	// InitInstance에 모든 중요한 초기화 작업을 배치합니다.
+}
+
+
+// 유일한 CExamMFCApp 개체입니다.
+
+CExamMFCApp theApp;
+
+
+// CExamMFCApp 초기화
+
+BOOL CExamMFCApp::InitInstance()
+{
+	CWinApp::InitInstance();
+
+
+	CExamMFCDlg dlg;
+	m_pMainWnd = &dlg;
+	dlg.DoModal();
+	
+	return FALSE;
+}
+
